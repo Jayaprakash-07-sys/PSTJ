080b08e638221ab688ae2cc5f00b650a077faac9
Jayaprakash-07-sys <vtu28024@veltech.edu.in>
2026-07-25 20:25:47 +0530

    Add files via upload

---
+++ CheckPalindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+interface PalindromeOperation {
+    boolean check(int n);
+}
+
+public class CheckPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        PalindromeOperation p = (n) -> {
+            int original = n;
+            int reverse = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                reverse = reverse * 10 + digit;
+                n /= 10;
+            }
+
+            return original == reverse;
+        };
+
+        if (p.check(number))
+            System.out.println(number + " is Palindrome");
+        else
+            System.out.println(number + " is Not Palindrome");
+
+        sc.close();
+    }
+}
