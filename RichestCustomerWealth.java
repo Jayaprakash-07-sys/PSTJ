df4456dc7d9c61628684609be5cff53f1d9107d5
Jayaprakash-07-sys <vtu28024@veltech.edu.in>
2026-07-27 12:31:13 +0530

    Add files via upload

---
+++ RichestCustomerWealth.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt(); // Number of customers
+        int n = sc.nextInt(); // Number of bank accounts
+
+        int[][] accounts = new int[m][n];
+
+        // Read account balances
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                accounts[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxWealth = 0;
+
+        // Calculate wealth of each customer
+        for (int i = 0; i < m; i++) {
+            int wealth = 0;
+
+            for (int j = 0; j < n; j++) {
+                wealth += accounts[i][j];
+            }
+
+            if (wealth > maxWealth) {
+                maxWealth = wealth;
+            }
+        }
+
+        System.out.println(maxWealth);
+
+        sc.close();
+    }
+}
