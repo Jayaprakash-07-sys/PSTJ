f80234e76b0094562423da942d3fa9fa5ccbfb70
Jayaprakash-07-sys <vtu28024@veltech.edu.in>
2026-07-28 12:04:27 +0530

    Add files via upload

---
+++ FindPivotIndex.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class FindPivotIndex {
+
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+
+        // Calculate total sum
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        // Find pivot index
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(pivotIndex(nums));
+
+        sc.close();
+    }
+}
